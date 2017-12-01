ce785d5b1fd4b6fad09d70055f80c29e4bd20277
MariaCamilaChac <32178494+MariaCamilaChac@users.noreply.github.com>
2017-12-01 02:52:21 -0500

    Add files via upload

--- Punto4.cpp
+++ Punto4.cpp
@@ -1,61 +1,81 @@
-/**Param:Matrix
- * Author: Maria Camila CHacon
- * Date:27/11/2017
- */
-
-#include<iostream>
+#include <vector>
 #include <string>
 #include <sstream>
+#include <iostream>
 
-using namespace std;
+ using namespace std;
 
-string NumberToString(int Number)
-{
- ostringstream oOStrStream;
- oOStrStream << Number;
- return oOStrStream.str();
-}
+ int main(){
 
-int main (int argc, char* argv[]){ //the statements that we going to use
+	 	int x;
+	  	string Cadena;
+	    vector<int> vect;
+	    cout <<"ingrese el tamano del arreglo: " << endl;
+	    cin >> x;
+	    int matriz[x*x];
+	    int matriz2[100][100];
+	    int tamain = (sizeof matriz/sizeof * matriz);
+	    cout << "Tamano es " << tamain << endl;
+	    cout << "Digite los numeros separado por comas: \n";
+	    cin >> Cadena;
+	    stringstream Separated(Cadena);
 
-int m = 0 ;
-cout << "ingrese el tamaÃ±o de la matriz: ";
-cin >> m ;
-int matriz [m][m]; 
-int matrix [m][m];
+	    int i;
 
-	for (int i=0;i<m;i++){  //iteration for create the array
-		for(int j=0;j<m;j++){
-		cout << "ingrese el dato, recuerde separar por comas: ";
-		cin >> matriz[i][j];
+	    while (Separated >> i){
+	         	 vect.push_back(i);
+	         if (Separated.peek() == ',')
+	        	 Separated.ignore();
+	    }
+
+	    int T = vect.size();
+	    cout << "Tamaño del vector " << T << endl;
+
+	    for (i=0; i< T; i++){
+
+	     	matriz[i] = vect.at(i);
+
+	    }
+
+	    cout<<endl;
+	    int c = 0;
+	    do{
+	    for (int i = 0; i < x; i++){
+
+			for (int j = 0; j < x ; j++) {
+
+				matriz2[i][j] = matriz[c];
+				c++;
+			}
+
 		}
-	}
-	
-	string vacio;
-	
-	for (int i=0;i<m ;i++){ //print the matrix 
-		vacio = "";
-		for (int j= 0 ;j<m ;j++){
-			vacio = vacio + "[" + NumberToString(matriz[i][j])+ "]"; //call the function
-		}
-		cout << vacio + "\n";	
-	}
-	
-	for (int i=0;i<m;i++){
-		for(int j=0;j<m;j++){
-	    	matrix [j][i]=matriz[j][i]; // fill the new matrix with the values of the old
-		}
-	}
-	
-	cout << "\n\n Matrix : \n\n";
-	
-	for (int i=0;i<m ;i++){
-		vacio = "";
-		for (int j= 0 ;j<m ;j++){
-			vacio = vacio + "[" + NumberToString(matrix[j][i])+ "]"; //print the matrix with cross values
-		}
-		cout << vacio + "\n";	
-	}
-}
 
+	  }while(c<tamain);
+
+	    for (int i = 0; i < x; i++) {
+	    	for (int j = 0; j < x; j++) {
+	    		cout << "["<<matriz2[i][j]<<"]";
+
+	    			}
+
+	    	cout<<endl;
+
+	    	}
+
+	    cout << endl;
+
+	    for (int i = 0; i < x; i++) {
+	    	    	for (int j = 0; j < x; j++) {
+	    	    		cout << "["<<matriz2[j][i]<<"]";
+
+	    	    			}
+
+	    	    	cout<<endl;
+
+	    	    	}
+
+	    system("PAUSE");
+	    return 0;
+
+ }
 
